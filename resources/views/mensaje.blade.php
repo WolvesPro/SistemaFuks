c95d5eed712c6638184baa02398d6d25addcb6bb
wolvesPro1 <44513545+wolvesPro1@users.noreply.github.com>
2018-11-05 18:43:31 -0600

    Add files via upload

---
+++ resources/views/mensaje.blade.php
@@ -0,0 +1,19 @@
+@extends('sistema.principal')
+
+@section('contenido')
+<div class="col-lg-6">
+<div class="card">
+<div class="card-header">
+</div>
+<br>
+<br>
+<br>
+<div class="card-body card-block">
+<H1>{{$proceso}}</h1>
+<br>
+<b>{{$mensaje}}</b>
+</div>
+</div>
+</div>
+</div>
+@stop
